5af53bd194870113142c4d5a1d421cbf8d9b0a26
Aazam Alam <aazam.alam.aa@gmail.com>
2025-09-27 22:38:51 -0400

    more suff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import StudentMaintenancePage from './components/StudentMaintenancePage';
 import Bulletin from './components/Bulletin';
+import SignupAdmin from "./components/SignupAdmin";
 
 function App() {
+  const handleLoginSuccess = () => {
+    // Handle successful login - navigate to admin dashboard or desired page
+    console.log('Admin login successful');
+    // You can add navigation logic here, like:
+    // window.location.href = '/admin-dashboard';
+    // or if you have an admin dashboard route, you could navigate there
+  };
+
   return (
     <Router>
       <div className="App">
@@ -12,6 +22,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/map/:userType" element={<StudentMaintenancePage />} />
           <Route path="/bulletin" element={<Bulletin />} />
+          <Route path="/SignupAdmin" element={<SignupAdmin onLoginSuccess={handleLoginSuccess} />} />
         </Routes>
       </div>
     </Router>
